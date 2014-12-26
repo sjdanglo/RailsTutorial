3c7c4fe30d22412c684ef4e16d2580858d7b2bd7
Sarah <sjdanglo@gmail.com>
2014-12-26 19:24:21 +0000

    12/26 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world!"
+    render text: "Hello, world!!!"
+  end
+  
+  def helloagain
+    render text: "Hello, again!"
   end
   
 end
